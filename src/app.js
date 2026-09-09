71964b2e79b2f7dbc755a31158aea7401f1262a7
Akash Mishra <mishraakash0718@gmail.com>
2026-09-09 12:02:05 +0530

    Api's

--- src/app.js
+++ src/app.js
@@ -12,6 +12,54 @@ connectDB().then(() => {
     })
 })
 
+//Get user by email id
+app.get('/user', async(req,res) => {
+    try {
+
+        // const userId = req.body.id;
+        // const user = await User.findById(userId);
+        // if(!user) {
+        //     res.status(404).send('User not found');
+        // } else {
+        //     res.send(user);
+        // }
+
+        // const userEmail = req.body.emailId;
+        // const user = await User.findOne({emailId: userEmail});
+        // if(!user) {
+        //     res.status(404).send('User not found');
+        // } else {
+        //     res.send(user);
+        // }
+
+
+        const userEmail = req.body.emailId;
+        const user = await User.find({emailId: userEmail});
+        if(user.length === 0) {
+            res.status(404).send('User not found');
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        res.status(500).send('Error fetching user');
+    }
+})
+
+//Get all users
+app.get('/feed', async(req,res) => {
+    try{
+        const users = await User.find();
+        if(users.length === 0) {
+            res.status(404).send('No users found');
+        } else{
+            res.send(users);
+        }
+    } catch(err) {
+        res.status(500).send('Error fetching users');
+    }
+})
+
+//Create a new user
 app.post('/signup', async(req, res) => {
     try {
         const user = new User(req.body);
@@ -22,3 +70,26 @@ app.post('/signup', async(req, res) => {
         res.status(500).send('Error creating user');
     }
 })
+
+//Update an user by id
+app.patch('/user', async(req, res) => {
+    try{
+        //Update using email id
+        // const userEmail = req.body.emailId;
+        // const {emailId, ...updateData} = req.body;
+        // const user = await User.findOneAndUpdate({emailId: userEmail}, updateData);
+        // console.log(user);
+        // res.send('User updated successfully');
+
+
+
+
+        const userId = req.body.userId;
+        const updateData = req.body;
+        const user = await User.findByIdAndUpdate(userId, updateData);
+        console.log(user);
+        res.send('User updated successfully');
+    }catch(err) {
+        res.status(500).send('Error updating user');
+    }
+});
