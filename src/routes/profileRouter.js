ce31ca5719506ca2e06ddcfb5e414314def76989
Akash Mishra <mishraakash0718@gmail.com>
2026-09-14 23:44:24 +0530

    Update-Password

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -3,6 +3,8 @@ const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { validatorForEditProfile } = require('../utils/validator');
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const Validator = require('validator');
 
 //Get profile
 profileRouter.get('/', userAuth, async(req, res) => {
@@ -26,10 +28,44 @@ profileRouter.post('/edit', userAuth, async(req, res) => {
 
         const updatedUser = await User.findOneAndUpdate({_id: loggedInUser._id}, req.body, {new: true, runValidators: true});
         res.json({message: "User has been updated successfully", user : updatedUser})
-
-
     }catch(err) {
         res.status(500).send('Error updating the profile : ' + err.message)
+    }
+})
+
+profileRouter.post('/password-update', userAuth, async(req,res) => {
+    try{
+        const {oldPassword, newPassword} = req.body;
+        if (!oldPassword || !newPassword) {
+            return res.status(400).send('Old password and new password are required');
+        }
+
+        const user = req.user;
+
+        //Check if old password is correct, then only allow to update the password
+        const isOldPasswordCorrect = await bcrypt.compare(oldPassword, user.password);
+        if(!isOldPasswordCorrect) {
+            return res.status(401).send('Incorrect old password');
+        }
+
+        //Check if the user is entering the same password again
+        const isNewPasswordSame = await bcrypt.compare(newPassword, user.password);
+        if(isNewPasswordSame){
+            return res.status(401).send('New password must be different from old password');
+        }
+
+        //Check if the new password is strong
+        if(!Validator.isStrongPassword(newPassword)) { 
+            return res.status(401).send('Enter a strong password');
+        }
+
+        const newHashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = newHashedPassword;
+        await user.save();
+
+        res.json({message : 'Password updated successfully'})
+    }catch(err){
+        res.status(500).send('ERROR : ' + err.message);
     }
 })
 
